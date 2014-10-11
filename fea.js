0f59e6a220c5f016d82bb049e3a360e3a7ca2502
NighterMan <jpenalbae@gmail.com>
2014-10-11 08:06:41 +0200

    Initial commit

---
+++ fea.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+// TODO
+//
+// - ROP Gadgets
+// - Search jumps and shit like that
+// - Format strings helper
+// - ROP chain debugger
+//
+
+var text = require('./lib/text');
+
+console.log(text.pattern(500));
+
